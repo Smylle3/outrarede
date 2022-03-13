2b82573e9cac450c43dbd7ae8c021428f3325a08
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-03-13 15:51:12 -0300

    Configura rotas iniciais

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+import Rotas from "routes/routes";
 
-export default App;
+export default function App() {
+  return <Rotas />;
+}
---
+++ src/routes/routes.js
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+import Login from 'pages/login'
+import SignIn from 'pages/signin'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Login/>} path="/" exact />
+                <Route element={<SignIn/>} path="/signin" />
+            </Routes>
+        </BrowserRouter>
+    )
+}
